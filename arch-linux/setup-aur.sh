45c3f7b6f6d6abac71b9c11653cf505bb8c2dbc9
The Bearodactyl <dabewodactowluwu@gmail.com>
2023-03-07 21:04:00 -0600

    Added M O R E

--- arch-linux/setup-aur.sh
+++ arch-linux/setup-aur.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
-sh ./chaotic-aur.sh
-echo "What is the AUR helper you'd like to install?"
-echo "[paru, yay, pikaur, aura, pakku trizen]"
-read -pr "--> " helper
-sudo pacman -S $helper
+if [ -f /etc/pacman.d/chaotic-mirrorlist ]
+then
+    echo "What is the AUR helper you'd like to install?"
+    echo "[paru, yay, pikaur, aura, pakku, trizen]"
+    read -r -p "--> " helper
+    sudo pacman -S $helper
+else
+    sh ./chaotic-aur.sh
+    echo "What is the AUR helper you'd like to install?"
+    echo "[paru, yay, pikaur, aura, pakku, trizen]"
+    read -r -p "--> " helper
+    sudo pacman -S $helper
+fi
